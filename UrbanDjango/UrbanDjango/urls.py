a0b131ea75a906f9cb0cb61a5a65ec9105415fd5
Zhuravleva0701 <shpachevamariia@gmail.com>
2024-10-09 19:54:31 +0300

    Добавлено

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from task2.views import func_template
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', func_template)
+    path('', func_template),
+    path('start/', TemplateView.as_view(template_name='second_task/class_template.html'))
 ]
