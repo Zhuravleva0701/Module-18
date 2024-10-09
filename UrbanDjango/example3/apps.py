55aee9ddfa78b7bca659ce6d570744d618f8f7cd
Zhuravleva0701 <shpachevamariia@gmail.com>
2024-10-09 12:29:29 +0300

    Добавлено

--- UrbanDjango/example3/apps.py
+++ UrbanDjango/example3/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Example1Config(AppConfig):
+class Example3Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'example1'
+    name = 'example3'
