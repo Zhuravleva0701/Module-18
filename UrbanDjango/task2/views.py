05717d15b40da0325ac28f8553476c5e3b0f9725
Zhuravleva0701 <shpachevamariia@gmail.com>
2024-11-22 10:41:39 +0300

    Добавлено

--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+
 # Create your views here.
 def func_template(request):
     return render(request, 'second_task/func_template.html')
